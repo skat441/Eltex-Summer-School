6a611bbd83f45aa5b0059a4f93e203f768bf55bb
skat441 <knovopashen44@gmail.com>
2025-07-21 11:38:41 +0700

    solve 6.3

--- module2/6/6.3/main.c
+++ module2/6/6.3/main.c
@@ -2,78 +2,78 @@
 #include <dlfcn.h>
 #include <stdlib.h>
 int main(){
-    void *sum_handle;
-    void *neg_handle;
-    void *mult_handle;
-    void *div_handle;
-    void *mod_handle;
+    // void *sum_handle;
+    // void *neg_handle;
+    // void *mult_handle;
+    // void *div_handle;
+    // void *mod_handle;
     void *select_handle;
     int answer=0;
     int a,b;
     int result;
-    int (*sum)(int, int );
-    int (*neg)(int, int );
-    int (*mult)(int, int );
-    int (*div)(int, int );
-    int (*mod)(int, int );
+    // int (*sum)(int, int );
+    // int (*neg)(int, int );
+    // int (*mult)(int, int );
+    // int (*div)(int, int );
+    // int (*mod)(int, int );
     int (*(*select)(int))(int, int);
     char *error;
-    sum_handle = dlopen ("libsum.so", RTLD_NOW);
-    if (!sum_handle) {
-        fputs (dlerror(), stderr);
-        exit(1);
-    }
-    neg_handle = dlopen ("libneg.so", RTLD_NOW);
-    if (!neg_handle) {
-        fputs (dlerror(), stderr);
-        exit(1);
-    }
-    mult_handle = dlopen ("libmult.so", RTLD_NOW);
-    if (!mult_handle) {
-        fputs (dlerror(), stderr);
-        exit(1);
-    }
-    div_handle = dlopen ("libdiv.so", RTLD_NOW);
-    if (!div_handle) {
-        fputs (dlerror(), stderr);
-        exit(1);
-    }
-    mod_handle = dlopen ("libmod.so", RTLD_NOW);
-    if (!mod_handle) {
-        fputs (dlerror(), stderr);
-        exit(1);
-    }
+    // sum_handle = dlopen ("libsum.so", RTLD_NOW);
+    // if (!sum_handle) {
+    //     fputs (dlerror(), stderr);
+    //     exit(1);
+    // }
+    // neg_handle = dlopen ("libneg.so", RTLD_NOW);
+    // if (!neg_handle) {
+    //     fputs (dlerror(), stderr);
+    //     exit(1);
+    // }
+    // mult_handle = dlopen ("libmult.so", RTLD_NOW);
+    // if (!mult_handle) {
+    //     fputs (dlerror(), stderr);
+    //     exit(1);
+    // }
+    // div_handle = dlopen ("libdiv.so", RTLD_NOW);
+    // if (!div_handle) {
+    //     fputs (dlerror(), stderr);
+    //     exit(1);
+    // }
+    // mod_handle = dlopen ("libmod.so", RTLD_NOW);
+    // if (!mod_handle) {
+    //     fputs (dlerror(), stderr);
+    //     exit(1);
+    // }
     select_handle = dlopen ("libselect.so", RTLD_NOW);
     if (!select_handle) {
         fputs (dlerror(), stderr);
         exit(1);
     }
-    sum = dlsym(sum_handle, "sum");
-    if ((error = dlerror()) != NULL) {
-        fprintf (stderr, "%s\n", error);
-        exit(1);
-    }
-    neg = dlsym(neg_handle, "neg");
-    if ((error = dlerror()) != NULL) {
-        fprintf (stderr, "%s\n", error);
-        exit(1);
-    }
-    mult = dlsym(mult_handle, "mult");
-    if ((error = dlerror()) != NULL) {
-        fprintf (stderr, "%s\n", error);
-        exit(1);
-    }
-    div = dlsym(div_handle, "div");
-    if ((error = dlerror()) != NULL) {
-        fprintf (stderr, "%s\n", error);
-        exit(1);
-    }
-    mod = dlsym(mod_handle, "mod");
-    if ((error = dlerror()) != NULL) {
-        fprintf (stderr, "%s\n", error);
-        exit(1);
-    }
-    select = dlsym(select, "select");
+    // sum = dlsym(sum_handle, "sum");
+    // if ((error = dlerror()) != NULL) {
+    //     fprintf (stderr, "%s\n", error);
+    //     exit(1);
+    // }
+    // neg = dlsym(neg_handle, "neg");
+    // if ((error = dlerror()) != NULL) {
+    //     fprintf (stderr, "%s\n", error);
+    //     exit(1);
+    // }
+    // mult = dlsym(mult_handle, "mult");
+    // if ((error = dlerror()) != NULL) {
+    //     fprintf (stderr, "%s\n", error);
+    //     exit(1);
+    // }
+    // div = dlsym(div_handle, "div");
+    // if ((error = dlerror()) != NULL) {
+    //     fprintf (stderr, "%s\n", error);
+    //     exit(1);
+    // }
+    // mod = dlsym(mod_handle, "mod");
+    // if ((error = dlerror()) != NULL) {
+    //     fprintf (stderr, "%s\n", error);
+    //     exit(1);
+    // }
+    select = dlsym(select_handle, "selected");
     if ((error = dlerror()) != NULL) {
         fprintf (stderr, "%s\n", error);
         exit(1);
@@ -92,11 +92,11 @@ int main(){
         result=operation(a,b);
         printf("Result is:%d",result);
     }
-    dlclose(sum_handle);
-    dlclose(neg_handle);
-    dlclose(mult_handle);
-    dlclose(div_handle);
-    dlclose(mod_handle);
-    dlclose(select_handle);
+    // dlclose(sum_handle);
+    // dlclose(neg_handle);
+    // dlclose(mult_handle);
+    // dlclose(div_handle);
+    // dlclose(mod_handle);
+    // dlclose(select_handle);
     return 0;
 }
--- module2/6/6.3/select.c
+++ module2/6/6.3/select.c
@@ -1,10 +1,69 @@
-#include "sum.c"
-#include "neg.c"
-#include "mult.c"
-#include "div.c"
-#include "mod.c"
+#include <stdio.h>
+#include <dlfcn.h>
+#include <stdlib.h>
 
-int (*select(int choice)) (int, int) {
+int (*selected(int choice)) (int, int) {
+    void *sum_handle;
+    void *neg_handle;
+    void *mult_handle;
+    void *div_handle;
+    void *mod_handle;
+    int (*sum)(int, int );
+    int (*neg)(int, int );
+    int (*mult)(int, int );
+    int (*div)(int, int );
+    int (*mod)(int, int );
+    char *error;
+    sum_handle = dlopen ("libsum.so", RTLD_NOW);
+    if (!sum_handle) {
+        fputs (dlerror(), stderr);
+        exit(1);
+    }
+    neg_handle = dlopen ("libneg.so", RTLD_NOW);
+    if (!neg_handle) {
+        fputs (dlerror(), stderr);
+        exit(1);
+    }
+    mult_handle = dlopen ("libmult.so", RTLD_NOW);
+    if (!mult_handle) {
+        fputs (dlerror(), stderr);
+        exit(1);
+    }
+    div_handle = dlopen ("libdiv.so", RTLD_NOW);
+    if (!div_handle) {
+        fputs (dlerror(), stderr);
+        exit(1);
+    }
+    mod_handle = dlopen ("libmod.so", RTLD_NOW);
+    if (!mod_handle) {
+        fputs (dlerror(), stderr);
+        exit(1);
+    }
+    sum = dlsym(sum_handle, "sum");
+    if ((error = dlerror()) != NULL) {
+        fprintf (stderr, "%s\n", error);
+        exit(1);
+    }
+    neg = dlsym(neg_handle, "neg");
+    if ((error = dlerror()) != NULL) {
+        fprintf (stderr, "%s\n", error);
+        exit(1);
+    }
+    mult = dlsym(mult_handle, "mult");
+    if ((error = dlerror()) != NULL) {
+        fprintf (stderr, "%s\n", error);
+        exit(1);
+    }
+    div = dlsym(div_handle, "div");
+    if ((error = dlerror()) != NULL) {
+        fprintf (stderr, "%s\n", error);
+        exit(1);
+    }
+    mod = dlsym(mod_handle, "mod");
+    if ((error = dlerror()) != NULL) {
+        fprintf (stderr, "%s\n", error);
+        exit(1);
+    }
     switch (choice) {
     case 1: return sum;
     case 2: return neg;
@@ -13,4 +72,9 @@ int (*select(int choice)) (int, int) {
     case 5: return mod;
     default: return sum;
     }
+    dlclose(sum_handle);
+    dlclose(neg_handle);
+    dlclose(mult_handle);
+    dlclose(div_handle);
+    dlclose(mod_handle);
 }
